fbc07c580f3cbe43b807844ea963c91c4546b2d0
patriciaalves1 <patricia.alves@gclick.com.br>
2024-09-21 13:59:46 -0300

    Iniciando projeto

---
+++ cypress/support/loginCommands.js
@@ -0,0 +1,21 @@
+const usuario = Cypress.env('username');
+const senha = Cypress.env('password');
+const usuarioErro = Cypress.env('usernameInvalido');
+const senhaErro = Cypress.env('passwordInvalida')
+
+Cypress.Commands.add('acessaPaginadeLogin', () => {
+    cy.visit('http://provaqa.prc.rpe.tech:9080/desafioqa/login');
+    cy.get('input[name="username"]').type(usuario);
+    cy.get('input[name="password"]').type(senha);
+    cy.get('button[type="submit"]').click();
+    cy.contains('Bem vindo ao Desafio').should('be.visible');
+    cy.url().should('include', '/inicio');
+});
+
+Cypress.Commands.add('acessaPaginadeLoginCredecialInvalida', () => {
+    cy.visit('http://provaqa.prc.rpe.tech:9080/desafioqa/login');
+    cy.get('input[name="username"]').type(usuarioErro);
+    cy.get('input[name="password"]').type(senhaErro);
+    cy.get('button[type="submit"]').click();
+    cy.contains('Credenciais inválidas').should('be.visible');
+});
